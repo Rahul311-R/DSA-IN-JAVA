c5d1d3432dbaba0bf5244fe179ce467a9886b8f5
Rahul311-R <rahul541r@gmail.com>
2025-08-27 19:09:32 +0530

    27/08/2025

---
+++ Day_2/src/leetcode_217.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class leetcode_217 {
+
+    static boolean isduplicate(int[] arr){
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: arr ){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of elements in array: ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println(isduplicate(arr));
+    }
+}
---
+++ Day_2/src/leetcode_219.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class leetcode_219 {
+
+    static boolean isduplicate1(int[] arr,int k){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<arr.length;i++ ){
+            if(set.contains(arr[i])){
+                return true;
+            }
+            set.add(arr[i]);
+        }
+        if (set.size() > k) {
+            set.remove(arr[i - k]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of elements in array: ");
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.print("enter the k value:");
+        int k = s.nextInt();
+        System.out.println(isduplicate1(arr,k));
+    }
+}
