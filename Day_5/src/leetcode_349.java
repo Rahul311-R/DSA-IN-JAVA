4446443513066e2eedfd49b297a26681edf8f121
Rahul R <rahul541r@gmail.com>
2025-09-02 15:49:24 +0530

    02/09/2025

--- Day_5/src/leetcode_349.java
+++ Day_5/src/leetcode_349.java
@@ -8,20 +8,17 @@ public class leetcode_349 {
             HashMap<Integer, Boolean> map = new HashMap<>();
             List<Integer> result = new ArrayList<>();
 
-            // put all nums1 elements into map
             for(int num : nums1) {
                 map.put(num, true);
             }
 
-            // check nums2 against map
             for(int num : nums2) {
                 if(map.containsKey(num)) {
                     result.add(num);
-                    map.remove(num); // ensures uniqueness
+                    map.remove(num);
                 }
             }
 
-            // convert List<Integer> to int[]
             int[] res = new int[result.size()];
             for(int i = 0; i < result.size(); i++) {
                 res[i] = result.get(i);
