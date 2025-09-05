b0f4f151a404e5caad11e04fcb6dea65b76f1b3a
Rahul R <rahul541r@gmail.com>
2025-09-05 11:47:48 +0530

    05/09/2025

---
+++ Day_8/src/leetcode_219.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class leetcode_219 {
+
+    static boolean Dupl(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (map.containsKey(num)) {
+                int prevIndex = map.get(num);
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+
+            // Update the index of current number
+            map.put(num, i);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0;i<n;i++){
+            nums[i] = s.nextInt();
+        }
+        int k = s.nextInt();
+
+        System.out.println(Dupl(nums,k));
+    }
+}
