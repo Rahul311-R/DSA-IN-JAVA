d739a8a7c99e424fb4c0704331d4381b9a36c136
Rahul R <rahul541r@gmail.com>
2025-09-10 09:45:41 +0530

    10/09/2025

--- Day_9/src/MergeSort.java
+++ Day_9/src/MergeSort.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class MergeSort {
+
+    static void merge(int[] arr, int i, int i1) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+
+        merge(arr,0,n-1);
+
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+    }
+
+
 }
