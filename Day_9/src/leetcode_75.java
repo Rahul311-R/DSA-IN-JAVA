e93fd13776e1df54f3995692d149adc4a471a03f
Rahul311-R <rahul541r@gmail.com>
2025-09-12 05:37:25 +0530

    12/09/2025

---
+++ Day_9/src/leetcode_75.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class leetcode_75 {
+    public static int[] sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        sortColors(arr);
+        for(int a : arr){
+            System.out.print(a+" ");
+        }
+    }
+}
